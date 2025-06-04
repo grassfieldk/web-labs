9c0687f6089be82be4ed1169fb84dd825f9ed062
TAGUSA Kentaro <tagusa-k@grassfield.online>
2025-06-04 18:12:19 +0900

    hololist | 基本リクエストを作成

---
+++ src/app/api/hololist/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import HololistService, { SearchParams } from "@/services/hololist/hololistService";
+
+export async function GET(req: NextRequest) {
+  try {
+    const searchParams: Partial<SearchParams> = {};
+    req.nextUrl.searchParams.forEach((value, key) => {
+      if (key in searchParams) {
+        searchParams[key as keyof SearchParams] = value;
+      }
+    });
+
+    const data = await HololistService.search(searchParams);
+
+    return new NextResponse(data, {
+      headers: { "content-type": "text/html; charset=utf-8" },
+    });
+  } catch (error) {
+    return new NextResponse(error instanceof Error ? error.message : "Internal Server Error", {
+      status: 500,
+    });
+  }
+}
