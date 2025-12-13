c748de1c0ec006b5d5c6182cf5f4f8d45fac6813
TAGUSA Kentaro <tagusa-k@grassfield.online>
2025-12-13 22:43:10 +0900

    フォントサイズの調整

--- src/components/AppProvider.tsx
+++ src/components/AppProvider.tsx
@@ -4,7 +4,18 @@ import { MantineProvider } from "@mantine/core";
 import { IconContext } from "react-icons";
 
 export const AppProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <MantineProvider defaultColorScheme="light">
+  <MantineProvider
+    defaultColorScheme="light"
+    theme={{
+      fontSizes: {
+        xs: "14px",
+        sm: "16px",
+        md: "16px",
+        lg: "18px",
+        xl: "20px",
+      },
+    }}
+  >
     <IconContext.Provider value={{ className: "inline-block" }}>
       {children}
     </IconContext.Provider>
